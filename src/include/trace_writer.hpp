56b1d7e970a2e88aa6e6364955c0c2e45ce01272
Christian Herold <harryherold@googlemail.com>
2020-02-10 21:57:36 +0100

    Implement first writer callbacks.

--- src/include/trace_writer.hpp
+++ src/include/trace_writer.hpp
@@ -5,7 +5,12 @@
 #include <string>
 #include <functional>
 
-#include <otf2/otf2.h>
+extern "C"
+{
+    #include <otf2/otf2.h>
+}
+
+#include "otf2_writer.hpp"
 
 using archive_deleter = std::function<void (OTF2_Archive *)>;
 using archive_ptr = std::unique_ptr<OTF2_Archive, archive_deleter>;
@@ -13,10 +18,7 @@ using archive_ptr = std::unique_ptr<OTF2_Archive, archive_deleter>;
 using event_writer_deleter = std::function<void (OTF2_EvtWriter *)>;
 using event_writer_ptr = std::unique_ptr<OTF2_EvtWriter, event_writer_deleter>;
 
-using def_writer_deleter = std::function<void (OTF2_GlobalDefWriter *)>;
-using def_writer_ptr = std::unique_ptr<OTF2_GlobalDefWriter, def_writer_deleter>;
-
-class TraceWriter {
+class TraceWriter: public Otf2Writer {
   public:
     /*
      * Initialize OTF2 archive, event and definition writer.
@@ -24,13 +26,37 @@ class TraceWriter {
      * @param path specify the folder of trace to be written
      */
     TraceWriter(const std::string &path);
-    ~TraceWriter();
+    virtual ~TraceWriter();
+
+    virtual void
+    writeClockProperties(uint64_t              timerResolution,
+                         uint64_t              globalOffset,
+                         uint64_t              traceLength );
+
+    virtual void
+    writeSystemTreeNode(OTF2_SystemTreeNodeRef self,
+                        OTF2_StringRef         name,
+                        OTF2_StringRef         className,
+                        OTF2_SystemTreeNodeRef parent );
+
+    virtual void
+    writeLocationGroup(OTF2_LocationGroupRef  self,
+                       OTF2_StringRef         name,
+                       OTF2_LocationGroupType locationGroupType,
+                       OTF2_SystemTreeNodeRef systemTreeParent );
+
+    virtual void
+    writeLocation(OTF2_LocationRef      self,
+                  OTF2_StringRef        name,
+                  OTF2_LocationType     locationType,
+                  uint64_t              numberOfEvents,
+                  OTF2_LocationGroupRef locationGroup );
 
   private:
     static OTF2_FlushCallbacks m_flush_callbacks;
     archive_ptr m_archive;
     event_writer_ptr m_event_writer;
-    def_writer_ptr m_def_writer;
+    OTF2_GlobalDefWriter* m_def_writer;
 };
 
 #endif /* TRACE_WRITER_H */
--- src/trace_writer.cpp
+++ src/trace_writer.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <trace_writer.hpp>
 
 OTF2_FlushType pre_flush(void *userData, OTF2_FileType fileType,
@@ -27,8 +28,8 @@ TraceWriter::TraceWriter(const std::string &path)
       m_event_writer(nullptr,
                      [this](OTF2_EvtWriter *writer) {
                          delete_event_writer(writer, m_archive.get());
-                     }),
-      m_def_writer(nullptr, [](OTF2_GlobalDefWriter *writer) {}) {
+                     })
+{
     auto *archive =
         OTF2_Archive_Open(path.c_str(), "trace", OTF2_FILEMODE_WRITE,
                           1024 * 1024 /* event chunk size */,
@@ -42,7 +43,56 @@ TraceWriter::TraceWriter(const std::string &path)
     OTF2_Archive_OpenEvtFiles(archive);
     m_event_writer.reset(OTF2_Archive_GetEvtWriter(archive, 0));
 
-    m_def_writer.reset(OTF2_Archive_GetGlobalDefWriter(archive));
+    m_def_writer = OTF2_Archive_GetGlobalDefWriter(archive);
+    assert(m_def_writer);
 }
 
 TraceWriter::~TraceWriter() {}
+
+void TraceWriter::writeClockProperties(uint64_t timerResolution,
+                                       uint64_t globalOffset,
+                                       uint64_t traceLength)
+{
+    OTF2_GlobalDefWriter_WriteClockProperties(m_def_writer,
+                                              timerResolution,
+                                              globalOffset,
+                                              traceLength);
+}
+
+void TraceWriter::writeSystemTreeNode(OTF2_SystemTreeNodeRef self,
+                                      OTF2_StringRef name,
+                                      OTF2_StringRef className,
+                                      OTF2_SystemTreeNodeRef parent) 
+{
+    OTF2_GlobalDefWriter_WriteSystemTreeNode(m_def_writer,
+                                             self,
+                                             name,
+                                             className,
+                                             parent);
+}
+
+void TraceWriter::writeLocationGroup(OTF2_LocationGroupRef self,
+                                     OTF2_StringRef name,
+                                     OTF2_LocationGroupType locationGroupType,
+                                     OTF2_SystemTreeNodeRef systemTreeParent) 
+{
+    OTF2_GlobalDefWriter_WriteLocationGroup(m_def_writer,
+                                            self,
+                                            name,
+                                            locationGroupType,
+                                            systemTreeParent);
+}
+
+void TraceWriter::writeLocation(OTF2_LocationRef self, 
+                                OTF2_StringRef name,
+                                OTF2_LocationType locationType,
+                                uint64_t numberOfEvents,
+                                OTF2_LocationGroupRef locationGroup) 
+{
+    OTF2_GlobalDefWriter_WriteLocation(m_def_writer,
+                                       self,
+                                       name,
+                                       locationType,
+                                       numberOfEvents,
+                                       locationGroup);
+}
