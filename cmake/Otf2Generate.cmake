866583b20dbe31d309f8efcc450fabbb30e4213c
Christian Herold <christian.herold@tu-dresden.de>
2020-10-21 16:24:29 +0200

    Add target for cleaning generated files.

--- cmake/Otf2Generate.cmake
+++ cmake/Otf2Generate.cmake
@@ -1,9 +1,15 @@
 add_custom_target(generate)
+add_custom_target(clean-generated)
 
 macro(generate_otf2_file target_name infile outfile)
     add_custom_target(${target_name}
-                    COMMAND otf2-template ${infile} ${outfile})
+                      COMMAND otf2-template ${infile} ${outfile})
 endmacro(generate_otf2_file)
+
+macro(add_genclean target_name file_path)
+    add_custom_target("${target_name}"
+                      COMMAND rm ${file_path})
+endmacro(add_genclean)
 
 macro(generate_otf2_files pattern substitute input_path output_path filelist)
     foreach(infilename ${filelist})
@@ -14,6 +20,10 @@ macro(generate_otf2_files pattern substitute input_path output_path filelist)
 
         generate_otf2_file(${outfilename} ${infile_path} ${outfile_path})
         add_dependencies(generate ${outfilename})
+
+        SET(clean_file_target "clean_${outfilename}")
+        add_genclean(${clean_file_target} "${CMAKE_CURRENT_SOURCE_DIR}/${output_path}/${outfilename}")
+        add_dependencies(clean-generated ${clean_file_target})
     endforeach()
 endmacro(generate_otf2_files)
 
